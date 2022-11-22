7895a1744df6bc3b693d67a27e4d0d32d9c089aa
github ParkBig <github enfantgu@gmail.com>
2022-11-22 18:44:03 +0900

    Friends.js 수정

--- src/pages/Friends.js
+++ src/pages/Friends.js
@@ -1,19 +1,30 @@
+import { motion } from "framer-motion";
 import { useState } from "react";
 import { useQuery } from "react-query";
+import styled from "styled-components";
 import { getMyInfo } from "../api/memberManage";
 import Loading from "../components/parkmade/common/loading/Loading";
+import Portal from "../components/parkmade/common/modal/Portal";
+import EditMyProfile from "../components/parkmade/common/navigationBar/EditMyProfile";
 import NavBar from "../components/parkmade/common/navigationBar/NavBar";
 
 
 const Friends = () => {
   const [isEditProfile, setIsEditProfile] = useState(false);
   const { isLoading:infoLoading, data:infoData } = useQuery(["myInfo"], getMyInfo);
+  // 요기서 리액트쿼리 쓰셔서 하위 컴포넌트로 뿌리시면 됩니다.
   return (
     <>
       {(infoLoading) ? 
         <Loading /> :
         <>
           <NavBar infoData={infoData} setIsEditProfile={setIsEditProfile}/>
+          <Portal>
+            {isEditProfile && <EditMyProfile info={infoData.data} setIsEditProfile={setIsEditProfile}/>}
+          </Portal>
+          <Wrap>
+            {/* 요기다가 컴포넌트 넣으면 됩니다. */}
+          </Wrap>
         </>
       } 
     </>
@@ -21,3 +32,11 @@ const Friends = () => {
 }
 
 export default Friends;
+
+const Wrap = styled(motion.div)`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3%;
+`;
